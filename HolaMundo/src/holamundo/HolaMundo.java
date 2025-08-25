0e726032ac2201b97f6a89ba20cf3fd362589dea
javie <javie@LAPTOP-0FI7GDPN.fibertel.com.ar>
2025-08-25 19:34:00 -0300

    primer commit

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,26 @@
+package holamundo;
+import java.util.ArrayList;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        
+        String nombre = "Juan";
+        int edad = 22;
+        double precioProducto = 25.33;
+        final int VALOR_ENTERO = 55;
+        
+        //Escalares
+        double numero = 12.33;
+        final int CONSTANTE = 6;
+        
+        //No escalares
+        int[] numeros = new int[25];
+        Padre objeto = new Padre();
+        
+       ArrayList<String> lista = new ArrayList<>();
+        
+        System.out.println("Hola mundo");
+       
+    }
+    
+}
